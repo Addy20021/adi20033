d735bd7e871f4419829a3864d244b7632eb443d1
Addy20021 <126099267+Addy20021@users.noreply.github.com>
2023-09-04 18:09:22 +0530

    Delete connection directory

--- connection/conn.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://adarsh:qHoSlcOi8wigawDR@cluster0.efowoij.mongodb.net/crudop?retryWrites=true&w=majority"
-).then(()=> console.log("connected"))
