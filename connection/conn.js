23e8592816123f3de7a5e43dc1cd5773e7892e6d
Addy20021 <126099267+Addy20021@users.noreply.github.com>
2023-09-04 18:06:39 +0530

    Add files via upload

---
+++ connection/conn.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://adarsh:qHoSlcOi8wigawDR@cluster0.efowoij.mongodb.net/crudop?retryWrites=true&w=majority"
+).then(()=> console.log("connected"))
